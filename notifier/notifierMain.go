7ee27f21c8099b427ad06474ecdd44f05ca99594
Bagaev Dmitry <dmitry.bagaev@7bits.it>
2022-08-25 18:19:18 +0600

    [*] trying to deal with issue on M1 mac;

--- notifier/notifierMain.go
+++ notifier/notifierMain.go
@@ -7,12 +7,10 @@ package notifier
 // #include "main.h"
 import "C"
 
-//StartNotifier starts the internal notifier function which communicates with the C library.
 func StartNotifier() {
 	C.registerNotifications()
 }
 
-//StopNotifier stops the internal notifier function which communicates with the C library.
 func StopNotifier() {
 	C.unregisterNotifications()
 }
